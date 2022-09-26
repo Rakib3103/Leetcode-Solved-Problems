8013c4bae989506bdf97f11a2c0ab8b5fe971806
Mazharul Islam Rakib <63310221+Rakib3103@users.noreply.github.com>
2022-09-26 18:12:07 +0600

    Add files via upload

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,19 +1,10 @@
-class Solution(object):
-    def romanToInt(self, s):
-        symbols = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500,'M':1000}
-        
-        l = len(s)
-        # print(l)
-        #Storing  the result
-        result = symbols[s[l - 1]]
-        # print(result)
-        #Checking with a for loop
-        for i in range(l - 2, -1, -1):
-            if symbols[s[i]] >= symbols[s[i+1]]:
-                result += symbols[s[i]]
-            else:
-                result -= symbols[s[i]]
-                
-        return result
-        
-        
+roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
+class Solution:
+    def romanToInt(self, S: str) -> int:
+        ans = 0
+        for i in range(len(S)-1,-1,-1):
+            num = roman[S[i]]
+            if 4 * num < ans: ans -= num
+            else: ans += num
+        return ans
